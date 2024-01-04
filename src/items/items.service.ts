a1a9f5c7cbe2cc1f9a124a444b8a15e01c328446
nobuki <nobking.63@gmail.com>
2024-01-04 13:17:23 +0900

    repository add

--- src/items/items.service.ts
+++ src/items/items.service.ts
@@ -1,41 +1,51 @@
 import { Injectable, NotFoundException } from '@nestjs/common';
-import { Item } from './item.model';
+import { Item } from '../entities/item.entity';
 import { ItemStatus } from './item-status.enum';
 import { CreateItemDto } from './dto/create-item.dto';
-import { v4 as uuid } from 'uuid';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
 
 @Injectable()
 export class ItemsService {
-  private items: Item[] = [];
-  findAll(): Item[] {
-    return this.items;
+  constructor(
+    @InjectRepository(Item) private readonly itemRepository: Repository<Item>,
+  ) {}
+  async findAll(): Promise<Item[]> {
+    return await this.itemRepository.find();
   }
 
-  findById(id: string): Item {
-    const found = this.items.find((item) => item.id === id);
+  async findById(id: string): Promise<Item> {
+    const found = await this.itemRepository.findOneBy({ id });
     if (!found) {
       throw new NotFoundException();
     }
     return found;
   }
 
-  create(createItemDto: CreateItemDto): Item {
-    const item: Item = {
-      id: uuid(),
-      ...createItemDto,
+  async create(createItemDto: CreateItemDto): Promise<Item> {
+    const { name, price, description } = createItemDto;
+    const item = this.itemRepository.create({
+      name,
+      price,
+      description,
       status: ItemStatus.ON_SALE,
-    };
-    this.items.push(item);
+      createdAt: new Date().toISOString(),
+      updatedAt: new Date().toISOString(),
+    });
+
+    await this.itemRepository.save(item);
     return item;
   }
 
-  updateStatus(id: string): Item {
-    const item = this.findById(id);
+  async updateStatus(id: string): Promise<Item> {
+    const item = await this.findById(id);
     item.status = ItemStatus.SOLD_OUT;
+    item.updatedAt = new Date().toISOString();
+    await this.itemRepository.save(item);
     return item;
   }
 
-  delete(id: string): void {
-    this.items = this.items.filter((item) => item.id !== id);
+  async delete(id: string): Promise<void> {
+    await this.itemRepository.delete({ id });
   }
 }
