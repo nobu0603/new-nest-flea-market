6baa431c0eb12a47cbb6ad0610cac0a7a880419d
nobuki <nobking.63@gmail.com>
2024-01-05 15:31:09 +0900

    test exsample add

---
+++ src/items/example.spec.ts
@@ -0,0 +1,9 @@
+import { describe } from 'node:test';
+
+describe('Jestの練習', () => {
+  it('test1', () => {
+    const result = 1;
+    const expected = 1;
+    expect(result).toEqual(expected);
+  });
+});
